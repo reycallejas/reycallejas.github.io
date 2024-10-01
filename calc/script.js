8545559903b893239462ae6368efb64b04e92d74
rey <160386432+reycallejas@users.noreply.github.com>
2024-09-30 23:56:55 -0600

    Create script.js

---
+++ calc/script.js
@@ -0,0 +1,98 @@
+const plus = document.getElementById('+')
+const minus = document.getElementById('-')
+const times = document.getElementById('x')
+const divide = document.getElementById('/')
+const equals = document.getElementById('=')
+const clear = document.getElementById('clear')
+
+let string = ''
+let displaystring = ''
+let answer = false
+
+// const test = '+23,45'
+// const testsliced = test.slice(1)
+// const split = testsliced.split(',')
+// console.log(typeof(split))
+// console.log(split)
+// console.log(Number(split[0]))
+const placeholder = document.getElementById('placeholder')
+
+for (let i=0;i<10;i++){
+    document.getElementById(i).onclick = () =>{
+        let input = document.getElementById(i).textContent
+        if (answer == false){
+            placeholder.style.display = 'none'
+            string = string + input
+            displaystring = displaystring + input
+            console.log(string)
+    }
+    }
+}
+
+
+
+plus.onclick = () => {
+    string = '+' + string + ','
+    displaystring = displaystring + ' + '
+    console.log(string)
+    answer = false
+}
+
+minus.onclick = () => {
+    string = '-' + string + ','
+    displaystring = displaystring + ' - '
+    answer = false
+}
+
+times.onclick = () => {
+    string = '*' + string + ','
+    displaystring = displaystring + ' x '
+    answer = false
+}
+
+divide.onclick = () => {
+    string = '/' + string + ','
+    displaystring = displaystring + ' / '
+    answer = false
+    
+}
+
+equals.onclick = () => {
+    let result = eval()
+    string = result
+    displaystring = result
+    console.log(displaystring)
+    answer = true
+    console.log(result)
+}
+
+function eval(){
+    const numbers = string.slice(1)
+    let numobj = numbers.split(',')
+    let n1 = Number(numobj[0])
+    let n2 = Number(numobj[1])
+    if (string[0] == '+'){
+        return(n1 + n2)
+    }else if(string[0] == '-'){
+        return(n1 - n2)
+    }else if(string[0] == '*'){
+        return(n1 * n2)
+    }else if(string[0] == '/'){
+        return(n1 / n2)
+    }
+}
+
+const interval = setInterval(render,100)
+
+const display = document.getElementById('display')
+
+function render(){
+    display.textContent=displaystring
+}
+
+clear.onclick = () => {
+    string = ''
+    displaystring = ''
+    answer = false
+    placeholder.style.display = 'block'
+}
